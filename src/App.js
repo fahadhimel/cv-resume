b8032d979a006054f417bf641108401ef340367a
fahadhimel <94831276+fahadhimel@users.noreply.github.com>
2024-05-01 15:58:43 +0600

    added particle animation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // fade slide---------------
 import Aos from "aos";
 import "aos/dist/aos.css";
+import ArticlesComponent from "./Components/particles/particlecomponents";
 
 function App() {
   // fade slide---------------
   useEffect(() => {
-    Aos.init({ duration: 3000 });
+    Aos.init({ duration: 2500 });
   }, []);
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:'#00855850',height:'50vh'}}>
+      <ArticlesComponent id="particle"/> 
       <Home />
     </div>
   );
